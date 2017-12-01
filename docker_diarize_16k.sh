ab565ae264a50087dc7639140dda77927a79c17a
WorkFit <sylvainlg@voicera.ai>
2017-11-30 16:09:56 -0800

    add pager duty integration

--- docker_diarize_16k.sh
+++ docker_diarize_16k.sh
@@ -5,6 +5,19 @@ OUTPUT_FOLDER=DiarizationOut
 BASE_NAME=$(basename $WAV_FILE)
 NAME="${BASE_NAME%.*}"
 
+# PD STUFF
+CONTENT_TYPE="application/json"
+DESCRIPTION="bad things™ are happening"
+URL="https://events.pagerduty.com/generic/2010-04-15/create_event.json"
+service_key=$PD_KEY
+
+function pd_trigger () {
+  curl -H "${CONTENT_TYPE}" \
+       -X POST \
+       -d "{ \"service_key\": \"$service_key\", \"event_type\": \"trigger\", \"description\": \"$1\" }" "${URL}"
+}
+
+
 echo '--- Check audio sample rate is 16kHz'
 is_16kHz=$(soxi $WAV_FILE |grep -i 'sample rate'|cut -f2 -d":"| xargs -I{} echo {} == 16000 | bc)
 if [ $is_16kHz -eq 1 ]; then
@@ -15,7 +28,9 @@ if [ $is_16kHz -eq 1 ]; then
     rm -rf $$OUTPUT_FOLDER
     exit 0
 else
-  echo '--- Diarization feature works only for 16kHz audio'
+  err_msg="[Diarization]: this feature works only for 16kHz audio for now !"
+  echo $err_msg
+  pd_trigger "$err_msg"
   exit 1
 fi
 
