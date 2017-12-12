659e83736e26f078de377e04d87785801e44258a
WorkFit <sylvainlg@voicera.ai>
2017-12-12 12:21:04 -0800

    convert mp3 to 16k wav when mp3 input

--- s3_diarize_16k.sh
+++ s3_diarize_16k.sh
@@ -2,26 +2,38 @@
 #
 # Usage:
 #$./s3_diarize_16k.sh s3://workfit-diarization-input-staging/input.wav  s3://workfit-diarization-output-staging
+#
 s3_input=$1
 s3_output=$2
 bn=$(basename $s3_input)
-fn=$(basename $s3_input .wav)
+dn=$(dirname $s3_input)
+
+#fn=$(basename $s3_input .wav)
+ext="${bn##*.}"
+fn="${bn%.*}"
+
+#echo 'basename:'$bn 'dirname:'$dn 'ext: '$ext 'filename: '$fn
+
 aws s3 cp $s3_input ./tmp
 
-#mkdir -p $outputfolder
+if [ $ext = 'mp3' ]; then
+   # resample to 16k and convert to wav
+   sox ./tmp/$bn --rate 16k ./tmp/$fn.wav
+   bn=$fn.wav
+   echo $bn
+fi
+
 
 ./ilp_diarization2.sh ./tmp/$bn 120 ./tmp/
 
 ./seg2json.py ./tmp/$bn ./tmp/$fn/$fn.ev_is.120.seg ./tmp/$fn/$fn.json ./tmp
 
 aws s3 cp ./tmp/$fn/$fn.json $s3_output
-#aws s3 cp ./tmp/*.wav $s3_output
+# TODO: uncomment if tyler is interested in getting audio file segments for UI
+# aws s3 cp ./tmp/*.wav $s3_output
 rm -rf ./tmp/*
 rm *.seg
  
 #why 120?
 #http://www.mickael-rouvier.fr/files/cross-show_diarization.pdf
-
-
-
 
